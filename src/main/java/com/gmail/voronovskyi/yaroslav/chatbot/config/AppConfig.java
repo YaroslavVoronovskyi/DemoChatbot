3d832c608f1e7f818001b62860b134e89ec955ca
yaroslav <yaroslav.voronovskyi@gmail.com>
2023-05-31 15:27:31 +0300

    Created simple telegram chatbot.

---
+++ src/main/java/com/gmail/voronovskyi/yaroslav/chatbot/config/AppConfig.java
@@ -0,0 +1,18 @@
+package com.gmail.voronovskyi.yaroslav.chatbot.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Data
+@Configuration
+@PropertySource("application.properties")
+public class AppConfig {
+
+    @Value("${bot.name}")
+    String botName;
+
+    @Value("${bot.token}")
+    String botToken;
+}
---
+++ src/main/java/com/gmail/voronovskyi/yaroslav/chatbot/service/TelegramBotService.java
@@ -0,0 +1,65 @@
+package com.gmail.voronovskyi.yaroslav.chatbot.service;
+
+import com.gmail.voronovskyi.yaroslav.chatbot.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBotService extends TelegramLongPollingBot {
+
+    private final AppConfig config;
+
+    @Autowired
+    public TelegramBotService(AppConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            String name = update.getMessage().getChat().getFirstName();
+
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, name);
+                    break;
+                default:
+                    sendMessage(chatId, "Sorry, command was not recognized ...");
+            }
+        }
+    }
+
+    private void startCommandReceived(long chatId, String name) {
+        String answer = "Hi, " + name + ", nice to meet you!";
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException exception) {
+            throw new RuntimeException("Error!");
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getBotToken();
+    }
+}
